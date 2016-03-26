2ad10ffe6e02810146bc011d19c184286f6a47c0
tony.liao.5966@gmail.com <tony.liao.5966@gmail.com>
2016-03-26 19:53:23 -0400

    copied some tutorials

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/public/javascripts/three.min.js', function(req, res){
+  res.sendFile(__dirname + '/public/javascripts/three.min.js');
+});
+
+app.get('/threeRender.js', function(req, res){
+  res.sendFile(__dirname + '/threeRender.js');
+});
+
 io.on('connection', function(socket){
 
   //myo stuff inside the connection
