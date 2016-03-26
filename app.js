216b11b70a73ca6edd3d40d44b513a1716c9a8b5
Broshen <boshen.cui@gmail.com>
2016-03-26 14:03:41 -0400

    myo updates now display on webpage... probaly...

--- app.js
+++ app.js
@@ -1,75 +1,23 @@
-// var express = require('express');
-// var path = require('path');
-// var favicon = require('serve-favicon');
-// var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
-// var bodyParser = require('body-parser');
 
-// var routes = require('./routes/index');
-// //var users = require('./routes/users');
-
-// var app = express();
-
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'hjs');
-
-// // uncomment after placing your favicon in /public
-// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(require('less-middleware')(path.join(__dirname, 'public')));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', routes);
-// //app.use('/users', users);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// // error handlers
-
-// // development error handler
-// // will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
-
-
-
-// module.exports = app;
-
+//app stuff
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+
+
+
+  //myo stuff inside the connection
 var Myo = require('myo');  
   var message='dickbutt';
   Myo.onError = function () {  
     console.log("Woah, couldn't connect to Myo Connect");
-    message="error";
-    io.emit('chat message', message);
+    message="Woah, couldn't connect to Myo Connect";
   }
 
   Myo.on('connected', function(){  
@@ -86,31 +34,38 @@ var Myo = require('myo');
     myo.on('fist', function(){
       console.log('fistin yer mum');
       message="fistin yer mum";
+
+      socket.emit('chat message', message);
+
     })
 
     myo.on('fingers_spread', function(){
       console.log('spreadin yer mum');
 
       message="spreadin yer mum";
+
+      socket.emit('chat message', message);
     })
 
-    myMyo.streamEMG(true);
-    myMyo.on('emg', function(data){
-      var datasum=0;
-      for(var i =0; i<8; i++){
-        datasum+=data[i];
-      }
-      console.log(datasum);
-    });
+    myo.on('wave_in', function(){
+      console.log('spreadin yer mum');
 
+      message="in ye go";
+
+      socket.emit('chat message', message);
+    })
+
+    myo.on('wave_out', function(){
+      console.log('spreadin yer mum');
+
+      message="out ye com";
+
+      socket.emit('chat message', message);
+    })
   }
 
   Myo.connect();
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
 
-io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
