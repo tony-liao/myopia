08d54d50fc1122b5aac9f6359184a5b2a17e0b75
Broshen <boshen.cui@gmail.com>
2016-03-26 12:29:51 -0400

    Switched to hoganjs, myo stuff moved into index.js

--- app.js
+++ app.js
@@ -4,16 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var Myo = require('myo');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+//var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hjs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,10 +20,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,19 +58,5 @@ app.use(function(err, req, res, next) {
 });
 
 
-
-//Myo testing
-Myo.on('connected', function(){
-  console.log('myo connected');
-});
-
-Myo.on('synced', function(){
-  console.log('myo synced'); //Doesn't work for some reason
-});
-
-Myo.on('fist', function(){
-  console.log('fist++');
-});
-Myo.connect();
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,62 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+var Myo = require('myo');  
+var message='dickbutt';
+Myo.onError = function () {  
+        console.log("Woah, couldn't connect to Myo Connect");
+        message="error";
+}
+
+Myo.on('connected', function(){  
+    console.log("TONY EATS ASS");
+
+        message="connected";
+    myMyo = this;
+
+    myMyo.streamEMG(true);
+    addEvents(myMyo);
 });
+
+var addEvents = function(myo){  
+    myo.on('fist', function(){
+        console.log('fistin yer mum');
+        message="fistin yer mum";
+    })
+    myo.on('fingers_spread', function(){
+        console.log('spreadin yer mum');
+
+        message="spreadin yer mum";
+    })
+    myMyo.streamEMG(true);
+myMyo.on('emg', function(data){
+	var datasum=0;
+	for(var i =0; i<8; i++){
+		datasum+=data[i];
+	}
+	if(datasum>10)
+    console.log("strong");
+	else
+		console.log("weak");
+});
+ //    myMyo.on('bluetooth_strength', function(val){
+ //    console.log('Such strength', val);
+	// });
+	// myMyo.requestBluetoothStrength();
+    
+
+
+}
+
+Myo.connect();
+
+
+router.get('/', function(req, res){
+	res.render('index', {status: message})
+})
 
 module.exports = router;
